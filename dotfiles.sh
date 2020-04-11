6e668bb9dca40172c903d91458bd0fbb3baac7e6
ghostbusker <austin9lee@gmail.com>
2020-04-11 03:28:55 -0400

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -13,8 +13,8 @@ c=$(( c < 70 ? 70 : c ))
 
 #set whiptail colors
 export NEWT_COLORS="
-root=,red
-roottext=white,red"
+root=,black
+roottext=green,black"
 
 #inform user and prompt for consent
 whiptail --backtitle "ghostbusker's dotfiles installer" \
@@ -24,7 +24,7 @@ whiptail --backtitle "ghostbusker's dotfiles installer" \
 ####FUCNTIONS#####
 
 chooseModules(){
-  echo "let user choose which modules to run"
+  echo "\e[101mINSTALLER:\e[92m let user choose which modules to run"
   MODULES=$(whiptail --backtitle "ghostbusker's dotfiles installer" \
     --title "Choose your own adventure" \
     --checklist --separate-output "Modules:" ${r} ${c} 20 \
@@ -62,17 +62,17 @@ chooseModules(){
 }
 
 checkRoot(){
-  echo "Checkinging for root..."
+  echo "\e[101mINSTALLER:\e[92m checkinging for root..."
   if [[ $EUID -eq 0 ]]; then
-      echo "You are root."
+      echo "you are root."
   else
-      echo "Please install sudo or run this as root."
+      echo "please install sudo or run this as root."
       exit 1
   fi
 }
 
 checkHelp(){
-  echo "checking for --help or -h flags"
+  echo "\e[101mINSTALLER:\e[92m checking for --help or -h flags"
   if [ ${#@} -ne 0 ] && [ "${@#"--help"}" = "" ]; then
     echo "there is no help."
     sleep 3
@@ -82,7 +82,7 @@ checkHelp(){
 }
 
 newEncryptedUser(){
-  echo "creating new encrypted user"
+  echo "\e[101mINSTALLER:\e[92m creating new encrypted user"
   USER=username
   USER=$(whiptail --backtitle "ghostbusker's dotfiles installer" \
   --title "New Encrypted User"  \
@@ -117,7 +117,7 @@ newEncryptedUser(){
 
 favoriteApps(){
   #This is going to get an implementation and organization overhaul
-  echo "installing favorite apps and tools"
+  echo "\e[101mINSTALLER:\e[92m installing favorite apps and tools"
   echo "installing termnial upgrade + terminal candy"
   sudo apt install -y terminator locate tilda neovim ranger trash-cli neofetch figlet \
   lolcat cmatrix hollywood funny-manpages caca-utils libaa-bin thefuck howdoi cowsay fortune
@@ -136,7 +136,7 @@ favoriteApps(){
 }
 
 desktopFromScratch (){
-  echo "installing graphical desktop environment"
+  echo "\e[101mINSTALLER:\e[92m installing graphical desktop environment"
   sudo apt install -y xorg xserver-xorg xinit git cmake lxappearance
   echo "installing i3-gaps window manager from source"
   cd /opt/ 
@@ -159,7 +159,7 @@ desktopFromScratch (){
 }
 
 makeFolders() {
-  echo "make folders in home directory"
+  echo "\e[101mINSTALLER:\e[92m make folders in home directory"
   sudo mkdir /home/$USER/Documents
   sudo mkdir /home/$USER/Downloads
   sudo mkdir /home/$USER/Music
@@ -168,7 +168,7 @@ makeFolders() {
 }
 
 scrapeWallpapers() {
-  echo "scraping wallpapers from the web" #shamelessly
+  echo "\e[101mINSTALLER:\e[92m scraping wallpapers from the web" #shamelessly
   sudo mkdir /home/$USER/Pictures/Wallpapers
   cd /home/$USER/Pictures/Wallpapers
   sudo wget http://getwallpapers.com/wallpaper/full/2/2/3/702223-free-rainforest-backgrounds-2560x1440.jpg
@@ -180,42 +180,42 @@ scrapeWallpapers() {
 }
 
 openVPN() {
-  echo "installing openvpn for tunneling back to home network"
+  echo "\e[101mINSTALLER:\e[92m installing openvpn for tunneling back to home network"
   sudo apt install -y openvpn
   #runs with: sudo openvpn ~./location/of/ovpn-file.ovpn
   #launched by i3, see ~/.config/i3/config
 }
 
 fixPiGroups() {
-  echo "adding user to all the groups that user pi was a part of"
+  echo "\e[101mINSTALLER:\e[92m adding user to all the groups that user pi was a part of"
   sudo usermod -a -G adm,dialout,cdrom,sudo,audio,video,plugdev,games,users,input,netdev,spi,i2c,gpio $USER
 }
 
 powerButtonLED() {
-  echo "adding GPIO pin3 shutdown to /boot/config.txt AND enabling UART so that pin 8 acts as power LED" 
+  echo "\e[101mINSTALLER:\e[92m adding GPIO pin3 shutdown to /boot/config.txt AND enabling UART so that pin 8 acts as power LED" 
   sudo sed -i '$a dtoverlay=gpio-shutdown,gpio_pin=3,active_low=1,gpio_pull=up\' /boot/config.txt
   sudo sed -i '$a enable_uart=1\' /boot/config.txt
 }
 
 localizeEasternUS() {
-  echo "setting localization for keyboard, clock, and wifi"
+  echo "\e[101mINSTALLER:\e[92m setting localization for keyboard, clock, and wifi"
   sudo sed -i 's/gb/us/g' /etc/default/keyboard
   sudo timedatectl set-timezone US/Eastern
   sudo raspi-config nonint do_wifi_country US
 }
 
 enableSSH() {
-  echo "enabling ssh as a service"
+  echo "\e[101mINSTALLER:\e[92m enabling ssh as a service"
   sudo raspi-config nonint do_ssh 0
 }
 
 enableVNC() {
-  echo "enabling vnc"
+  echo "\e[101mINSTALLER:\e[92m enabling vnc"
   sudo raspi-config nonint do_vnc 0
 }
 
 retroPie() {
-  echo "installing retropie"
+  echo "\e[101mINSTALLER:\e[92m installing retropie"
   cd /opt/
   sudo git clone --depth=1 https://github.com/RetroPie/RetroPie-Setup.git
   cd RetroPie-Setup
@@ -223,12 +223,12 @@ retroPie() {
 }
 
 creativeSuite() {
-  echo "installing ghostbusker's creative suite"
+  echo "\e[101mINSTALLER:\e[92m installing ghostbusker's creative suite"
   sudo apt install -y mixxx kdenlive blender audacity gimp
 }
 
 coolRetroTerm() {
-  echo "installing cool retro terminal"
+  echo "\e[101mINSTALLER:\e[92m installing cool retro terminal"
   cd /opt/ 
   sudo apt install -y build-essential qmlscene qt5-qmake qt5-default qtdeclarative5-dev qml-module-qtquick-controls \
   qml-module-qtgraphicaleffects qml-module-qtquick-dialogs qml-module-qtquick-localstorage qml-module-qtquick-window2 \
@@ -241,7 +241,7 @@ coolRetroTerm() {
 }
 
 terminalPipes() {
-  echo "installing pipeseroni's pipes.sh from source"
+  echo "\e[101mINSTALLER:\e[92m installing pipeseroni's pipes.sh from source"
   cd /opt/
   sudo git clone https://github.com/pipeseroni/pipes.sh
   cd pipes.sh
@@ -259,7 +259,7 @@ terminalPipes() {
 #probs done installing NMmatrix  
 
 asciiAquarium(){
-  echo "installing ascii aquarium"
+  echo "\e[101mINSTALLER:\e[92m installing ascii aquarium"
   sudo apt-get install libcurses-perl
   cd /opt/ 
   sudo wget http://search.cpan.org/CPAN/authors/id/K/KB/KBAUCOM/Term-Animation-2.4.tar.gz
@@ -276,7 +276,7 @@ asciiAquarium(){
 }
 
 swapfileChange(){
-  echo "adjusting sawp file size"
+  echo "\e[101mINSTALLER:\e[92m adjusting sawp file size"
   sudo sed -i 's/^CONF_SWAPSIZE=[0-9]*$/CONF_SWAPSIZE=512/' /etc/dphys-swapfile
   sudo dphys-swapfile setup
   #Andreas Speiss recomends these swap file changes
@@ -291,7 +291,7 @@ swapfileChange(){
 }
 
 log2Ram() {
-  echo "installing log2ram" #must be done last and requires reboot
+  echo "\e[101mINSTALLER:\e[92m installing log2ram" #must be done last and requires reboot
   printf -- 'deb http://packages.azlux.fr/debian/ buster main'| sudo tee /etc/apt/sources.list.d/azlux.list
   wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
   apt update
@@ -299,7 +299,7 @@ log2Ram() {
 }
 
 copyDotfiles (){
-  echo "copying dotfiles into place"
+  echo "\e[101mINSTALLER:\e[92m copying dotfiles into place"
   sudo cp -r .config/ /home/$USER/
   sudo cp -r .bashrc /home/$USER/
 
@@ -311,16 +311,18 @@ copyDotfiles (){
 }
 
 deleteUserPi(){
-  echo "deleting pi user and removing all files"
+  echo "\e[101mINSTALLER:\e[92m deleting pi user and removing all files"
   sudo pkill -u pi
   sudo userdel --remove-all-files pi
 }
 
 pepareInstall(){
-  echo "preparing to run modules"
+  echo "\e[101mINSTALLER:\e[92m preparing to run modules"
   sudo git config --global color.ui auto
   echo "updating package list"
   sudo apt update || sudo apt-get -o Acquire::ForceIPv4=true update
+  echo "updating git"
+  sudo apt install -y git
   echo "starting script timer"
   STOPWATCH=0
   echo "creating install log"
@@ -332,19 +334,19 @@ pepareInstall(){
 }
 
 runModules(){
-  echo "running each module in sequence"
+  echo "\e[101mINSTALLER:\e[92m running each module in sequence"
   for module in $MODULES ; do \
     $module | tee -a -i $tmpLog ; \
   done
 }
 
 exportLog(){
-  echo "exporting log to currrent working directory"
+  echo "\e[101mINSTALLER:\e[92m exporting log to currrent working directory"
   sudo mv $tmpLog $(pwd)
 }
 
 showInfo(){
-  echo "showing some helpful info"
+  echo "\e[101mINSTALLER:\e[92m showing some helpful info"
   echo "Current /boot/config.txt settings: "
   vcgencmd get_config int
   echo "showing directory tree"
