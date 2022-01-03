e9b3568d6ecbeea8ba145983b957267db5446a17
ghostbusker <austin9lee@gmail.com>
2022-01-03 00:30:42 -0500

    Update newdotfiles.sh

--- newdotfiles.sh
+++ newdotfiles.sh
@@ -90,15 +90,16 @@ desktop_from_scratch () {
   sudo apt -y --fix-missing install i3 gcc make autoconf dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev \
   libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
   libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+  sudo apt -y --fix-missing install meson ninja dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev \
+  libxcb-icccm4-dev  libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
+  libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
   sudo git clone https://www.github.com/Airblader/i3 i3-gaps
   cd i3-gaps
-  sudo autoreconf --force --install
-  sudo rm -rf build/
-  sudo mkdir -p build 
-  cd build/
-  sudo ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-  sudo make -j8
-  sudo make install
+  # compile
+  mkdir -p build && cd build
+  meson ..
+  ninja
+  sudo ninja install
   #installing apps that will be part of desktop composition and daily use
   sudo apt -y --fix-missing install i3blocks feh compton clipit arandr mpv florence dunst nemo conky dhcpcd-gtk wpagui
   #installing wifi and bluetooth tools"
@@ -400,7 +401,7 @@ sudo git config --global color.ui auto
 # magic loop that calls each fucntion, anounces its name, executes it, and logs the output
 for module in $MODULES ; do \
   printf "\nINSTALLER: RUNNING MODULE $module \n" | tee -a -i $TEMPLOG
-  $module | tee -a -i $TEMPLOG ; \
+  $module | tee -a -i $TEMPLOG ;
 done
 
 # stop script timer
