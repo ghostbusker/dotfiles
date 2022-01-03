a25469240d64f2c39c5d163c3772d5c96f4191e0
ghostbusker <austin9lee@gmail.com>
2022-01-03 01:07:46 -0500

    Update newdotfiles.sh

--- newdotfiles.sh
+++ newdotfiles.sh
@@ -90,9 +90,8 @@ desktop_from_scratch () {
   sudo apt -y --fix-missing install i3 gcc make autoconf dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev \
   libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
   libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
-  sudo apt -y --fix-missing install meson ninja dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev \
-  libxcb-icccm4-dev  libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev \
-  libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+  sudo apt -y --fix-missing install meson python3-pip
+  sudo pip install ninja
   sudo git clone https://www.github.com/Airblader/i3 i3-gaps
   cd i3-gaps
   # compile
